cd5b4fa6c031429193ae76a7330d76c895b8563d
Nawaf A Al-Harbi <nawaf.manage@gmail.com>
2020-11-27 17:57:05 +0300

    update..

--- Assignment/Assignment.java
+++ Assignment/Assignment.java
@@ -2,29 +2,37 @@ import java.util.Scanner;
 
 public class Assignment {
     public static void main(String[] args) {
+        DisplayAll();
+    }
 
-        /*
-         * String[] menu = new String[5]; menu[0] = "pizza"; menu[1] = "Burger"; menu[2]
-         * = "IceCream"; menu[3] = "Sushi"; menu[4] = "Fries";
-         */
-        int orderNum;
-        int iOrders = 0;
-        Boolean Terminator = true;
-        String [] orders = new String[30];
-
+    
+    public static String[] DisplayMenu() {
         System.out.println("========== Hadeel's resturant ==========");
         System.out.println("Please make a selection...");
         String[] menu = { "", "Pizza", "Burger", "IceCream", "Sushi", "Fries" };
-
         for (int i = 0; i < menu.length; i++) {
             if (i == 0) {
             } else {
                 System.out.println(i + "." + menu[i]);
             }
         }
+        return menu;
+    }
+
+    public static String[] GetUserOrders() {
+        Scanner sizeScanner = new Scanner(System.in);
+        Scanner orderScanner = new Scanner(System.in);
+        int orderNum;
+        int iOrders = 0;
+        String[] menu = DisplayMenu();
+
+        System.out.println("How many Orders Do you want to place?");
+        int size = sizeScanner.nextInt();
+        String[] orders = new String[size];
+        System.out.println("Please enter the numner of each of your " + size + " orders .. \n Press 0 to exit anytime");
 
         do {
-            orderNum = GetUserInput();
+            orderNum = orderScanner.nextInt();
             if (orderNum == 1) {
                 orders[iOrders] = menu[orderNum];
             } else if (orderNum == 2) {
@@ -36,31 +44,33 @@ public class Assignment {
             } else if (orderNum == 5) {
                 orders[iOrders] = menu[orderNum];
             }
-          iOrders++;
-          System.out.print ("IOrders = " + iOrders);
-        } while (orderNum != 0);
 
-        System.out.println ("The user has ordered the following..."); 
+            System.out.println("You Ordered: " + orders[iOrders]);
+            iOrders++;
+            if (iOrders < size) {
+                System.out.println("Please Proceed with the next order");
+            }
+        } while (orderNum != 0 && iOrders < size);
+        return orders;
+    }
+
+    public static String GetUserAddress() {
+        Scanner addressScanner = new Scanner(System.in);
+        System.out.println("Please write your address: ");
+        String address = addressScanner.nextLine();
+        return address;
+    }
+
+    public static void DisplayAll () {
+        String[] orders = GetUserOrders();
+        String address = GetUserAddress();
+        System.out.println("============= Recipt =============");
+        System.out.println("Your order contains the following...");
         for (int i = 0; i < orders.length; i++) {
             System.out.println(orders[i]);
-        }
+        }        
+        System.out.println("Your address is: " + address);
+        System.out.println("=============== End ===============");
 
-        /*
-         * Scanner keyboard = new Scanner(System.in);
-         * System.out.println("Enter your adress: "); String adress
-         * =keyboard.nextLine(); System.out.println("your order: " + menu [order-1] +
-         * " will be deliverd to the adress: " + adress);
-         * 
-         */
     }
-
-    public static int GetUserInput() {
-
-        Scanner input = new Scanner(System.in);
-        System.out.println("Please enter the num of you order/s .. \n Press 0 when done");
-        int order = input.nextInt();
-        return order;
-    }
-
 }
-
